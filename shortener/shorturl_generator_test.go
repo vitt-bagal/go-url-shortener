101528abe478329499882218f1c16268f2b2310b
Vitthal Bagal <vitthalb@us.ibm.com>
2022-02-17 14:00:04 +0530

    Added test

--- shortener/shorturl_generator_test.go
+++ shortener/shorturl_generator_test.go
@@ -2,6 +2,8 @@ package shortener
 
 import (
 	"testing"
+
+	"github.com/vitt-bagal/go-url-shortener/store"
 )
 
 func TestGenerateShortURL(t *testing.T) {
@@ -12,12 +14,12 @@ func TestGenerateShortURL(t *testing.T) {
 	}
 }
 
-/*func TestSameLongURL(t *testing.T) {
-	long_url := handler.UrlRequest{"https://www.guru3d.com/news-story/philips-monitors-launches-three-new-pc-gaming-monitors-from-its-m3000-series.html"}
-	actualString := store.StoreToFile("http://localhost:9090/GFbHs4PY", long_url.LongUrl)
+func TestSameLongURL(t *testing.T) {
+	long_url := "https://www.guru3d.com/news-story/philips-monitors-launches-three-new-pc-gaming-monitors-from-its-m3000-series.html"
+	actualString := store.StoreToFile("http://localhost:9090/GFbHs4PY", long_url)
 	expectedString := "http://localhost:9090/GFbHs4PY"
 	if actualString != expectedString {
 		t.Errorf("Expected String(%s) is not same as"+
 			" actual string (%s)", expectedString, actualString)
 	}
-}*/
+}
